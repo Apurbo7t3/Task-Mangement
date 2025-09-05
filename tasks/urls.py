08bbcd4e3cd176e02e865c922b3a51a45c7d626e
Apurbo <apurbo4t4@gmail.com>
2025-09-06 01:33:52 +0600

    dynamic url added

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from tasks.views import showTask
+from tasks.views import showTask,showSpecificTask
 urlpatterns = [
     path('show-task/',showTask),
+    path('show-task/<int:id>/',showSpecificTask),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -5,3 +5,5 @@ def home(request):
     return HttpResponse("<h1 style='color:red'>This is Home Page</h1>")
 def showTask(request):
     return HttpResponse('This is task page')
+def showSpecificTask(request,id):
+    return HttpResponse(f"This is Specific Task {id}")
